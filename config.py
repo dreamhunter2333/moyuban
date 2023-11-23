964be2707fef1de6c86b2253525aa46f366ff505
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-11-23 12:35:37 +0800

    init

--- config.py
+++ config.py
@@ -11,9 +11,6 @@ logging.basicConfig(
 
 class Settings(BaseSettings):
     holidays_file: str = "data/holidays.json"
-    holidays_file_url: str = "https://raw.githubusercontent.com/dreamhunter2333/moyuban/main/data/holidays.json"
-    holidays_file_cron: str = "0 0 * * 1"
-    holidays_lazy_update_interval_days: int = 7
 
     class Config:
         env_file = ".env"
