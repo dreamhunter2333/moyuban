30179b0449b8d02efb57d077985f1f45e18fd81b
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-11-27 16:43:12 +0800

    feat: support pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: '摸鱼办',
+        short_name: '摸鱼办',
+        description: '摸鱼办 - 一个集成各种工具的网站',
+        icons: [
+          {
+            src: '/images/pwa.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
