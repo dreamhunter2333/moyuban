4a14c0ada4d1f5476438e362c0d4f2fe4c8000ee
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-12-01 16:42:36 +0800

    fix: base64 error in url

--- frontend/src/views/ShareView.vue
+++ frontend/src/views/ShareView.vue
@@ -3,7 +3,6 @@ import { NRadioButton, NRadioGroup, NModal } from 'naive-ui';
 import { NInput, NCard, NButton, useMessage } from 'naive-ui';
 import useClipboard from 'vue-clipboard3'
 import { useRoute } from 'vue-router'
-import { Base64 } from 'js-base64';
 import LZString from 'lz-string';
 import MarkdownIt from 'markdown-it';
 import QrcodeVue from 'qrcode.vue'
@@ -19,13 +18,11 @@ const content = ref('');
 const contentType = ref('markdown');
 const isShared = ref(false);
 const showQrcode = ref(false);
-const sharedURL = ref('');
 
 if (route.query && route.query.content && route.query.contentType) {
   contentType.value = route.query.contentType;
   try {
-    const compressedContent = Base64.decode(route.query.content);
-    content.value = LZString.decompress(compressedContent);
+    content.value = LZString.decompressFromEncodedURIComponent(route.query.content);
   } catch (e) {
     message.error('解码失败: ' + e.message);
   }
@@ -33,11 +30,10 @@ if (route.query && route.query.content && route.query.contentType) {
 }
 
 const generateSharedURL = () => {
-  const compressedContent = LZString.compress(content.value);
-  const base64Str = Base64.encode(compressedContent);
+  const compressedContent = LZString.compressToEncodedURIComponent(content.value);
   return `${window.location.origin}/share`
     + `?contentType=${contentType.value}`
-    + `&content=${base64Str}`
+    + `&content=${compressedContent}`
 }
 
 const share = async () => {
@@ -50,6 +46,9 @@ const share = async () => {
 };
 
 const markdownRender = (content) => {
+  if (!content) {
+    return '';
+  }
   try {
     return md.render(content);
   } catch (e) {
