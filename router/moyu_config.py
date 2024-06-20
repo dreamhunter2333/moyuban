188803524b299edf558a062078a61471c070521b
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-06-20 13:30:35 +0800

    feat: update moyu template

--- router/moyu_config.py
+++ router/moyu_config.py
@@ -22,12 +22,12 @@ MO_YU_TEMPLATE = """
 
 ## 【工资】
 
- - 距离【月底发工资】: {salaryday1} 天
- - 距离【05号发工资】: {salaryday5} 天
- - 距离【10号发工资】: {salaryday10} 天
- - 距离【15号发工资】: {salaryday15} 天
- - 距离【20号发工资】: {salaryday20} 天
- - 距离【周六】还有 {day_to_weekend} 天
+- 距离【月底发工资】: {salaryday1} 天
+- 距离【05号发工资】: {salaryday5} 天
+- 距离【10号发工资】: {salaryday10} 天
+- 距离【15号发工资】: {salaryday15} 天
+- 距离【20号发工资】: {salaryday20} 天
+- 距离【周六】还有 {day_to_weekend} 天
 """
 
 MO_YU_TEMPLATE_DAY_N = """
@@ -41,8 +41,8 @@ MO_YU_TEMPLATE_DAY_N = """
 
 ## 【工资】
 
- - 距离【 {salaryday} 号发工资】: {salarydayn} 天
- - 距离【周六】还有 {day_to_weekend} 天
+- 距离【 {salaryday} 号发工资】: {salarydayn} 天
+- 距离【周六】还有 {day_to_weekend} 天
 """
 
 HOLIDAY_TITLE = """
@@ -53,20 +53,20 @@ MO_YU_TEMPLATE_TXT = """【摸鱼办】提醒您:
 {year} 年 已经过去 {passdays} 天 {passhours} 小时, 今天是 {year}年{month}月{day}日, 星期{weekday}
 你好, 摸鱼人！工作再忙, 一定不要忘记摸鱼哦！有事没事起身去茶水间, 去厕所, 去走廊走走, 去找同事聊聊八卦别老在工位上坐着, 钱是老板的但命是自己的。
 【工资】
- - 距离【月底发工资】: {salaryday1} 天
- - 距离【05号发工资】: {salaryday5} 天
- - 距离【10号发工资】: {salaryday10} 天
- - 距离【15号发工资】: {salaryday15} 天
- - 距离【20号发工资】: {salaryday20} 天
- - 距离【周六】还有 {day_to_weekend} 天"""
+- 距离【月底发工资】: {salaryday1} 天
+- 距离【05号发工资】: {salaryday5} 天
+- 距离【10号发工资】: {salaryday10} 天
+- 距离【15号发工资】: {salaryday15} 天
+- 距离【20号发工资】: {salaryday20} 天
+- 距离【周六】还有 {day_to_weekend} 天"""
 
 MO_YU_TEMPLATE_DAY_N_TXT = """
 【摸鱼办】提醒您:
 {year} 年 已经过去 {passdays} 天 {passhours} 小时, 今天是 {year}年{month}月{day}日, 星期{weekday}
 你好, 摸鱼人！工作再忙, 一定不要忘记摸鱼哦！有事没事起身去茶水间, 去厕所, 去走廊走走, 去找同事聊聊八卦别老在工位上坐着, 钱是老板的但命是自己的。
 【工资】
- - 距离【 {salaryday} 号发工资】: {salarydayn} 天
- - 距离【周六】还有 {day_to_weekend} 天"""
+- 距离【 {salaryday} 号发工资】: {salarydayn} 天
+- 距离【周六】还有 {day_to_weekend} 天"""
 
 PALU_TEMPLATE = """
 # 【帕鲁综合办】提醒您: {year} 年 已经过去 {passdays} 天 {passhours} 小时
@@ -79,12 +79,12 @@ PALU_TEMPLATE = """
 
 ## 【工资】
 
- - 距离【月底发工资】: {salaryday1} 天
- - 距离【05号发工资】: {salaryday5} 天
- - 距离【10号发工资】: {salaryday10} 天
- - 距离【15号发工资】: {salaryday15} 天
- - 距离【20号发工资】: {salaryday20} 天
- - 距离【周六】还有 {day_to_weekend} 天
+- 距离【月底发工资】: {salaryday1} 天
+- 距离【05号发工资】: {salaryday5} 天
+- 距离【10号发工资】: {salaryday10} 天
+- 距离【15号发工资】: {salaryday15} 天
+- 距离【20号发工资】: {salaryday20} 天
+- 距离【周六】还有 {day_to_weekend} 天
 """
 
 PALU_TEMPLATE_DAY_N = """
@@ -98,8 +98,8 @@ PALU_TEMPLATE_DAY_N = """
 
 ## 【工资】
 
- - 距离【 {salaryday} 号发工资】: {salarydayn} 天
- - 距离【周六】还有 {day_to_weekend} 天
+- 距离【 {salaryday} 号发工资】: {salarydayn} 天
+- 距离【周六】还有 {day_to_weekend} 天
 """
 
 PALU_TEMPLATE_TIPS = """
